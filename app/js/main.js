8c162a442205ffca9186753dc7e25914de1fc012
Paweł Pavlov <pavlov1space@gmail.com>
2020-05-18 23:25:25 +0200

    fixed bg video

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
-
-
-
-  
+$(document).ready(function () {
+    var videobackground = new $.backgroundVideo($('body'), {
+        "align": "centerXY",
+        "width": 1280,
+        "height": 720,
+        "path": "video/",
+        "filename": "bg",
+        "types": ["mp4", "ogv", "webm"],
+        "preload": true,
+        "autoplay": true,
+        "loop": true
+    });
 });
