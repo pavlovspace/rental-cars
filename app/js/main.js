50f1fea26bb2964ac7d8738ef72a95310bad3b90
Paweł Pavlov <pavlov1space@gmail.com>
2020-05-19 12:12:24 +0200

    implemented vanta.js

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+VANTA.RINGS({
+    el: "#header",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x0,
+    color: 0xff00c3
+})
