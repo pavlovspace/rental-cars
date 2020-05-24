ff0fd650d7ba8b254a063e21b72dcc5dced9b47d
Paweł Pavlov <pavlov1space@gmail.com>
2020-05-24 15:47:15 +0200

    implemented slick slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,20 @@
-VANTA.RINGS({
-    el: "#header",
-    mouseControls: true,
-    touchControls: true,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    backgroundColor: 0x0,
-    color: 0xff00c3
-})
+$(function () {
+    VANTA.RINGS({
+            el: "#header",
+            mouseControls: true,
+            touchControls: true,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            scale: 1.00,
+            scaleMobile: 1.00,
+            backgroundColor: 0x0,
+            color: 0xff00c3
+        }),
+
+        $('.statistics-slider').slick({
+            dots: true,
+            infinite: true,
+            arrows: false,
+            autoplay: 3000,
+        });
+});
