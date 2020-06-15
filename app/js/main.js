f8340b1e1ebb6b186b3027131ea1e0b0546dfc94
Paweł Pavlov <pavlov1space@gmail.com>
2020-06-15 16:46:46 +0200

    implemented `scroll to top` and fixed adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,17 @@ let burgerClick = () => {
   menu.classList.toggle("menu--active");
 };
 closeBtn.addEventListener("click", burgerClick);
+
+// scroll to top
+const scrollTop = document.querySelector('.scroll-top');
+
+window.onscroll = () => {
+  if (window.scrollY > 700) {
+    scrollTop.classList.remove('scroll-top--hide')
+  } else if (window.scrollY < 700) {
+    scrollTop.classList.add('scroll-top--hide')
+  }
+}
+scrollTop.onclick = () => {
+  window.scrollTo(0, 0);
+}
