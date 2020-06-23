e69c6ecca5fd62dc26a002185fddfa2c79d89381
Paweł Pavlov <pavlov1space@gmail.com>
2020-06-23 15:55:26 +0200

    implemented popup

--- app/js/main.js
+++ app/js/main.js
@@ -9,43 +9,57 @@ let burgerClick = () => {
 closeBtn.addEventListener("click", burgerClick);
 
 // -- scroll to top
-const scrollTop = document.querySelector('.scroll-top');
+const scrollTop = document.querySelector(".scroll-top");
 
 window.onscroll = () => {
   if (window.scrollY > 700) {
-    scrollTop.classList.remove('scroll-top--hide')
+    scrollTop.classList.remove("scroll-top--hide");
   } else if (window.scrollY < 700) {
-    scrollTop.classList.add('scroll-top--hide')
+    scrollTop.classList.add("scroll-top--hide");
   }
-}
+};
 scrollTop.onclick = () => {
   window.scrollTo(0, 0);
-}
+};
 
 // -- tabs
 let tab = function () {
-  let tabNav = document.querySelectorAll('.tabs-nav__item'),
-    tabContent = document.querySelectorAll('.tab'),
+  let tabNav = document.querySelectorAll(".tabs-nav__item"),
+    tabContent = document.querySelectorAll(".tab"),
     tabName;
 
-  tabNav.forEach(item => {
-    item.addEventListener('click', selectTabNav)
+  tabNav.forEach((item) => {
+    item.addEventListener("click", selectTabNav);
   });
 
   function selectTabNav() {
-    tabNav.forEach(item => {
-      item.classList.remove('is-active');
+    tabNav.forEach((item) => {
+      item.classList.remove("is-active");
     });
-    this.classList.add('is-active');
-    tabName = this.getAttribute('data-tab-name');
+    this.classList.add("is-active");
+    tabName = this.getAttribute("data-tab-name");
     selectTabContent(tabName);
   }
 
   function selectTabContent(tabName) {
-    tabContent.forEach(item => {
-      item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
-    })
+    tabContent.forEach((item) => {
+      item.classList.contains(tabName)
+        ? item.classList.add("is-active")
+        : item.classList.remove("is-active");
+    });
   }
-
 };
 tab();
+
+// -- popup
+const closeP = document.querySelector("#closeP");
+const popupNone = document.querySelector("#popup-none");
+const popBtn = document.querySelector("#popBtn");
+
+popBtn.addEventListener("click", () => {
+  popupNone.style.display = "block";
+});
+
+closeP.addEventListener("click", () => {
+  popupNone.style.display = "none";
+});
